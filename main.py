ce471da0b575be8dddc450b0ffc9e9522352547b
Khalid <khaliduddin@gmail.com>
2023-11-20 09:08:23 +0000

    testing yfinance package with macd

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('hello codespace')
+import pandas_ta as ta
+import yfinance as yf
+
+# Request historic pricing data via finance.yahoo.com API
+df = yf.Ticker('BTC-USD').history(period='1y')[['Close', 'Open', 'High', 'Volume']]
+
+# Calculate MACD values using the pandas_ta library
+df.ta.macd(close='close', fast=12, slow=26, signal=9, append=True)
+
+print(df)
